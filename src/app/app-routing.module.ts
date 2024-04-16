3319968a4e26473caaf43bfa7d5a939dff0d2590
Erik Halás <34719696+ErikHalas@users.noreply.github.com>
2024-04-16 18:47:39 +0200

    fix-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {HeaderComponent} from "./header/header.component";
 
 
 const routes: Routes = [
-  { path: '', component: WelcomePageComponent }
+  { path: '', component: WelcomePageComponent, pathMatch: 'full' },
+  { path: 'daco', component: HeaderComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
