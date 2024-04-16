dfa65019753b6ac8065649588569f9fee7e1b41b
Erik Halás <erik.halas@artinsolutions.com>
2024-04-16 14:06:41 +0200

    added mainPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoHeaderComponent } from './logo-header/logo-header.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { GenericButtonComponent } from './generic-button/generic-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoHeaderComponent
+    LogoHeaderComponent,
+    LoginComponentComponent,
+    WelcomePageComponent,
+    GenericButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/generic-button/generic-button.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-generic-button',
+  templateUrl: './generic-button.component.html',
+  styleUrls: ['./generic-button.component.scss']
+})
+export class GenericButtonComponent {
+  constructor() { }
+
+  @Input() text = '';
+}
